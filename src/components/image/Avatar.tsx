dbc317067da09cb3ada25fafb11563574203750a
oyunsanaachat-cpu <oyunsanaachat@gmail.com>
2025-11-02 00:57:09 +0800

    Update Avatar.tsx

--- src/components/image/Avatar.tsx
+++ src/components/image/Avatar.tsx
@@ -1,41 +1,44 @@
 'use client'
+
 import type { ResponsiveValue } from "@chakra-ui/react";
 import type * as CSS from "csstype";
-import { chakra, useColorMode } from '@chakra-ui/system';
-import { ComponentProps } from 'react';
+import { chakra, useColorMode } from "@chakra-ui/system";
+import { ComponentProps } from "react";
 import { Image } from './Image';
 
 type AvatarImageProps = Partial<
-    ComponentProps<typeof Image> & {
-        showBorder?: boolean;
-    }
+  ComponentProps<typeof Image> & {
+    showBorder?: boolean;
+  }
 >;
 
 export function NextAvatar({
-    src,
-    showBorder,
-    alt = '',
-    style,
-    ...props
+  src,
+  showBorder,
+  alt = '',
+  style,
+  ...props
 }: AvatarImageProps) {
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
 
-    return (
-        <Image
-  {...props}
-  {...(showBorder
-    ? {
-        border: '2px',
-        borderColor: colorMode === 'dark' ? 'navy.700' : 'white',
-      }
-    : {})}
-  alt={alt}
- const objectFit: "cover" | "contain" | "fill" | "none" | "scale-down" = "cover";
-  objectFit={objectFit}
-  src={src}
-  style={{ ...style, borderRadius: '50%' }}
-/>
-    );
+  // ✅ objectFit тодорхойлж өгч байна
+  const objectFit: ResponsiveValue<CSS.Property.ObjectFit> = 'cover';
+
+  return (
+    <Image
+      {...props}
+      {...(showBorder
+        ? {
+            border: '2px',
+            borderColor: colorMode === 'dark' ? 'navy.700' : 'white',
+          }
+        : {})}
+      alt={alt}
+      objectFit={objectFit}
+      src={src}
+      style={{ ...style, borderRadius: '50%' }}
+    />
+  );
 }
 
 export const ChakraNextAvatar = chakra(NextAvatar, {
